2fd9de7eb0847a905a00d9881ff6da53f547a190
Toxyxer <toxyxer@gmail.com>
2016-10-06 10:49:08 +0200

    physics: Collision.ts update

--- src/physics/Collisions.ts
+++ src/physics/Collisions.ts
@@ -1,34 +1,60 @@
-import {Vec2} from "../util/Vec2";
+import {Vec2} from "../util/Vec2"
 
+enum Shape {
+	Rectangular,
+	Circle
+}
 
-
-class bigCircle {
-	constructor(public radius: number, public position: Vec2) {
-	}
+interface Rectangular {
+	shape: Shape;
+	max: Vec2;
+	min: Vec2;
 }
 
 interface Circle{
+	shape: Shape;
 	radius: number;
 	position: Vec2;
-
 }
 
-function isIntersecting(lhs, rhs): boolean {
-	// if(lhs instanceof Circle) {
-		// code...
-	// }
-	return true;
+export function areIntersecting(lhs: any, rhs: any): boolean {
+
+	if(lhs.shape == Shape.Circle && rhs.shape == Shape.Circle) {
+		return circleCircleIntersecting(lhs, rhs);
+	}
+
+	else if(lhs.shape == Shape.Circle && rhs.shape == Shape.Rectangular) {
+		return circleRectangularIntersecting(lhs, rhs);
+	}
+
+	else if(lhs.shape == Shape.Rectangular && rhs.shape == Shape.Circle) {
+		return circleRectangularIntersecting(rhs, lhs);
+	}
+
+	else if(lhs.shape == Shape.Rectangular && rhs.shape == Shape.Rectangular) {
+		rectangularRectangularIntersecting(lhs, rhs);
+	}
+
+	else {
+		throw "Can't check intersections between these objects.";	
+	}
 }
 
-/*function isIntersecting(lhs :Rectangular, rhs :Circle) {
-	console.log(lhs.position, rhs.position);
+function circleRectangularIntersecting(lhs: Circle, rhs: Rectangular): boolean {
+	return true; // TODO
 }
-*/
+
+function circleCircleIntersecting(lhs: Circle, rhs: Circle): boolean {
+	return lhs.position.distance(rhs.position) <= lhs.radius + rhs.radius;
+}
+
+function rectangularRectangularIntersecting(lhs: Rectangular, rhs: Rectangular): boolean {
+	return true; // TODO
+}
 
 
-let x: bigCircle = new bigCircle(5, new Vec2(1,1));
-let y: bigCircle = new bigCircle(2, new Vec2(0,0));
+let x: Circle = {shape: Shape.Circle, radius: 5, position: new Vec2(0,0)};
+let y: Circle = {shape: Shape.Circle, radius: 5, position: new Vec2(0,11)};
 
-// console.log(instanceof bigCircle)
 
-isIntersecting(x, y);
+console.log(areIntersecting(x, y));
