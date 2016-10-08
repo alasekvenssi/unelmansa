a84cda2b123eadff16db26a2114748a0216fde36
Teapot418 <teapot404@gmail.com>
2016-10-08 21:43:48 +0200

    graphics: RenderGroup, RenderTransform

--- src/graphics/Renderable.ts
+++ src/graphics/Renderable.ts
@@ -1,5 +1,27 @@
 import {Context2D} from "./Context2D"
+import TransformMatrix from "../util/TransformMatrix"
 
 export interface Renderable {
 	render(context: Context2D): void;
+}
+
+export class RenderTransform implements Renderable {
+	constructor(public transform: TransformMatrix, public item: Renderable) {}
+
+	render(ctx: Context2D) {
+		let old = ctx.transformMatrix();
+		ctx.transform(this.transform);
+		this.item.render(ctx);
+		ctx.transformMatrix(old);
+	}
+}
+
+export class RenderGroup implements Renderable {
+	items = new Array<Renderable>();
+
+	render(ctx: Context2D) {
+		for (let item of this.items) {
+			item.render(ctx);
+		}
+	}
 }
