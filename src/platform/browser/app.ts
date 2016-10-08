0c72c5aef63f6d465dc255348f9d4d68817c26fa
Teapot418 <teapot404@gmail.com>
2016-10-08 21:44:03 +0200

    platform/browser: Demo

--- src/platform/browser/app.ts
+++ src/platform/browser/app.ts
@@ -4,30 +4,37 @@ import CanvasContext2D from "../../graphics/browser/CanvasContext2D"
 import WebImage from "../../graphics/browser/WebImage"
 import Color from "../../util/Color"
 import {Font} from "../../util/Font"
-
-let view = new CanvasWindow(window, 2);
-let ctx = view.context;
-
-let angle = 0;
-let lastTime = Date.now();
-setInterval(() => { angle += (Math.PI / 360)*2; }, 20);
+import TransformMatrix from "../../util/TransformMatrix"
+import Renderer from "../../graphics/Renderer"
+import {Renderable, RenderGroup, RenderTransform} from "../../graphics/Renderable"
 
 let img = new WebImage("test.jpg");
 
-function draw() {
-	ctx.reset().fillColor(Color.Yellow).strokeColor(Color.Black).lineWidth(3).alpha(0.95);
+class TestRect implements Renderable {
+	render(ctx: Context2D) {
+		ctx.fillColor(Color.Yellow).strokeColor(Color.Black).lineWidth(1);
+		ctx.alpha(0.95).drawRect(0, 0, 100, 100, true, true).alpha(0.5).drawImage(img, 0, 0, 100, 100);
+	}
+};
 
-	for (let x = 0; x <= 25; x++) {
-		for (let y = 0; y <= 25; y++) {
-			ctx.resetTransform().translate(200*x, 200*y).rotate(angle*(((x+y)%2)*2-1)).drawRect(0, 0, 200, 200, true, true);
-			ctx.alpha(0.5).drawImage(img, 0, 0, 200, 200).alpha(0.95);
+let rotation = new TransformMatrix();
+let group = new RenderGroup();
+
+for (let x = 0; x <= 25; x++) {
+	for (let y = 0; y <= 25; y++) {
+		group.items[group.items.length] = new RenderTransform(TransformMatrix.translate(100*x, 100*y),
+			new RenderTransform(rotation, new TestRect()));
+	}
+}
+
+let view = new CanvasWindow(window, 2);
+new Renderer(view.context, group, 2, true);
+
+setInterval(() => {
+	let matrix = rotation.mul(TransformMatrix.rotate((Math.PI / 360)*2));
+	for (let y = 0; y < 2; y++) {
+		for (let x = 0; x < 3; x++) {
+			rotation[y][x] = matrix[y][x];
 		}
 	}
-	requestAnimationFrame(draw);
-
-	let now = Date.now();
-	ctx.resetTransform().alpha(1).fillColor(Color.Black).font(new Font("Courier New", 50));
-	ctx.drawText(10, 10, `${Math.round(1000/(now-lastTime))} fps`, "hanging", true, false);
-	lastTime = now;
-}
-draw();
+}, 20);
