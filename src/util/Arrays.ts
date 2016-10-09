0f8580dc9c38214b9b8eff8aad32979c5ac2a498
Teapot418 <teapot404@gmail.com>
2016-10-09 10:33:12 +0200

    util: fillArray

--- src/util/Arrays.ts
+++ src/util/Arrays.ts
@@ -4,3 +4,10 @@ export function forEachInPlace<T>(array: T[], op: ForEachType<T>): T[] {
 	array.forEach(op);
 	return array;
 }
+
+export function fillArray<T>(array: T[], begin: number, end: number, value: T): T[] {
+	for (let i = begin; i < end; i++) {
+		array[i] = value;
+	}
+	return array;
+}
