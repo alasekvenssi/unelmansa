f9d548f33851a30373759a77ec42a12ee659daa1
Teapot418 <teapot404@gmail.com>
2016-10-11 21:24:41 +0200

    util: Array filter, filterInPlace

--- src/util/Arrays.ts
+++ src/util/Arrays.ts
@@ -1,4 +1,5 @@
 type ForEachType<T> = (value: T, index: number, array: T[]) => void;
+type ForEachFilterType<T> = (value: T, index: number, array: T[]) => boolean;
 
 export function forEachInPlace<T>(array: T[], op: ForEachType<T>): T[] {
 	array.forEach(op);
@@ -10,4 +11,25 @@ export function fillArray<T>(array: T[], begin: number, end: number, value: T): 
 		array[i] = value;
 	}
 	return array;
+}
+
+export function filterInPlace<T>(array: T[], filter: ForEachFilterType<T>): T[] {
+	for (let i = 0; i < array.length; i++) {
+		if (!filter(array[i], i, array)) {
+			array.splice(i, 1);
+			i--;
+		}
+	}
+	return array;
+}
+
+export function filter<T>(array: T[], filter: ForEachFilterType<T>): T[] {
+	let filtered = new Array<T>();
+
+	for (let i = 0; i < array.length; i++) {
+		if (filter(array[i], i, array)) {
+			filtered.push(array[i]);
+		}
+	}
+	return filtered;
 }
