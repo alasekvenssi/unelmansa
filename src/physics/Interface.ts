6fc84e01ebbd6be0a7629cb948e81a8badb9da94
Teapot418 <teapot404@gmail.com>
2016-10-11 21:06:24 +0200

    physics: New Simulable interface

--- src/physics/Interface.ts
+++ src/physics/Interface.ts
@@ -2,6 +2,20 @@ import Vec2 from "../util/Vec2";
 import Intersections = require("./Intersections");
 
 export interface Simulable {
+	mass: number;
+	elasticity: number;
+	friction: number;
+
+	velocity: Vec2;
+	acceleration: Vec2;
+
+	bounding(): any;
+	movable(): boolean;
+
+	affect(physical: Simulable[]): void;
+}
+
+export interface Simulable2 {
 	shape: Intersections.Shape;
 
 	mass: number;
