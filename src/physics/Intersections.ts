8c18b0998a58b3f70c243d4aba2034dbda605833
Toxyxer <toxyxer@gmail.com>
2016-10-12 09:16:45 +0200

    physics: Updated Intersections to match with new Simulable

--- src/physics/Intersections.ts
+++ src/physics/Intersections.ts
@@ -1,63 +1,63 @@
 import Vec2 from "../util/Vec2"
 
-export enum Shape {
-	AABB,
-	Circle,
-	Muscle
+abstract class Bounding {}
+
+class Circle extends Bounding{
+	constructor(
+		public position: Vec2,
+		public radius: number) {
+		super()
+	}
 }
 
-interface BoundingBox {
-	readonly shape: Shape;
-	
-	readonly min?: Vec2;
-	readonly max?: Vec2;
-
-	readonly radius?: number;
-	readonly position?: Vec2;
+class AABB extends Bounding{
+	constructor(
+		public min: Vec2,
+		public max: Vec2) {
+		super()
+	}
 }
 
-
-export function areIntersecting(lhs: BoundingBox, rhs: BoundingBox): boolean {
-	
-	if(lhs.shape == Shape.Muscle || rhs.shape == Shape.Muscle) {
+export function areIntersecting(lhs: Bounding, rhs: Bounding): boolean {
+	if(lhs == undefined || rhs == undefined) {
 		return false;
 	}
 
-	else if(lhs.shape == Shape.Circle && rhs.shape == Shape.Circle) {
+	else if((lhs instanceof Circle) && (rhs instanceof Circle)) {
 		return circleVsCircle(lhs, rhs);
 	}
 
-	else if(lhs.shape == Shape.Circle && rhs.shape == Shape.AABB) {
+	else if(lhs instanceof Circle && rhs instanceof AABB) {
 		return circleVsAABB(lhs, rhs);
 	}
 
-	else if(lhs.shape == Shape.AABB && rhs.shape == Shape.Circle) {
+	else if(lhs instanceof AABB && rhs instanceof Circle) {
 		return circleVsAABB(rhs, lhs);
 	}
 
-	else if(lhs.shape == Shape.AABB && rhs.shape == Shape.AABB) {
+	else if(lhs instanceof AABB && rhs instanceof AABB) {
 		return AABBVsAABB(lhs, rhs);
 	}
 
 	else {
 		throw "Can't check intersections between these objects.";	
 	}
+	
 }
 
-function circleVsAABB(lhs: BoundingBox, rhs: BoundingBox): boolean {
-	let CircleAABB :BoundingBox = {
-		shape: Shape.AABB, 
+function circleVsAABB(lhs: Circle, rhs: AABB): boolean {
+	let CircleAABB :AABB = {
 		min: lhs.position.sub(new Vec2(lhs.radius, lhs.radius)), 
 		max: lhs.position.add(new Vec2(lhs.radius, lhs.radius))
 	};
 	return AABBVsAABB(CircleAABB, rhs);
 }
 
-function circleVsCircle(lhs: BoundingBox, rhs: BoundingBox): boolean {
+function circleVsCircle(lhs: Circle, rhs: Circle): boolean {
 	return lhs.position.distance(rhs.position) <= lhs.radius + rhs.radius;
 }
 
-function AABBVsAABB(lhs: BoundingBox, rhs: BoundingBox): boolean {
+function AABBVsAABB(lhs: AABB, rhs: AABB): boolean {
 
 	if(lhs.max.x < rhs.min.x || lhs.min.x > rhs.max.x) {
 		return false;
