d05769af50e454e24446f83a93f791223382ef52
Teapot418 <teapot404@gmail.com>
2016-10-13 13:15:19 +0200

    platform/browser: Disabled centering camera on creature

--- src/platform/browser/app.ts
+++ src/platform/browser/app.ts
@@ -36,11 +36,11 @@ let renderer = new Renderer(view.context, camera, 1, true);
 
 renderer.start();
 
+let center = stefan.center();
+
 setInterval(
 	() => {
 		scene.update(1/60);
-
-		let pos = stefan.center();
-		camera.transform = TransformMatrix.translate(view.width()/2-pos.x, view.height() - 150);
+		camera.transform = TransformMatrix.translate(view.width()/2-center.x, view.height() - 150);
 	}, 1000/60
 );
