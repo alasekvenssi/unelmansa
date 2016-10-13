513e92d9dec72374c85af91924ec3a8d12750f8c
Teapot418 <teapot404@gmail.com>
2016-10-13 10:09:37 +0200

    core: Added air resistance and markers on ground

--- src/core/Entity.ts
+++ src/core/Entity.ts
@@ -1,5 +1,6 @@
 import Vec2 from "../util/Vec2"
 import Color from "../util/Color"
+import {Font} from "../util/Font"
 import {Renderable} from "../graphics/Renderable"
 import {Context2D} from "../graphics/Context2D"
 import {Simulable} from "../physics/Interface"
@@ -42,13 +43,32 @@ export class Ground extends Entity {
 		for (let affectedObject of affectedObjects) {
 			if(affectedObject.movable()) {
 				affectedObject.acceleration = affectedObject.acceleration.add(new Vec2(0,-1000));
-				console.log(affectedObject.acceleration);
-				// affectedObject.position = affectedObject.position.sub(Intersections.intersectionDelta(affectedObject.bounding(), this.bounding()));
 			}
 		}
 	}
 
 	render(context: Context2D): void {
 		context.fillRGBA(0, 127, 0).drawRect(-100000, -100000, 200000, 100000, true, false);
+		for (let i = -10; i <= 10; i++) {
+			context.fillColor(Color.Black).drawRect(i*200-1, -100000, 2, 100000, true, false);
+
+			context.save().translate(i*200-1, 0).scale(1, -1);
+			context.font(new Font("Arial", 30)).drawText(12, 12, i.toString(), "top", true, false);
+			context.restore();
+		}
+	}
+}
+
+export class Air extends Entity {
+	constructor(public resistance: number = 1) {
+		super();
+	}
+
+	affect(objects: Simulable[]): void {
+		for (let obj of objects) {
+			if(obj.movable()) {
+				obj.acceleration = obj.acceleration.sub(obj.velocity.mul(this.resistance));
+			}
+		}
 	}
 }
