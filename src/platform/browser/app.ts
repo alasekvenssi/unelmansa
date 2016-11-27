23326544715e8c251a869635ca908ee5d7dbb8b6
Teapot418 <teapot404@gmail.com>
2016-11-27 23:14:48 +0100

    platform/browser: Show SimulationView

--- src/platform/browser/app.ts
+++ src/platform/browser/app.ts
@@ -9,37 +9,11 @@ import Vec2 from "../../util/Vec2"
 import Color from "../../util/Color"
 import WebImage from "../../graphics/browser/WebImage"
 import {InteractiveContext2D} from "../../graphics/InteractiveContext2D"
+import SimulationView from "../../ui/SimulationView"
 
-let stefan = new Creature();
+let simView = new SimulationView();
 
-stefan.bones.push(new CreatureBone(new Vec2(-100, 780), 40, 2, 0.5, 0.5));
-stefan.bones.push(new CreatureBone(new Vec2(170, 950), 40, 1, 0.5, 0.5));
-stefan.bones.push(new CreatureBone(new Vec2(400, 710), 40, 1, 0.5, 0.5));
-stefan.bones.push(new CreatureBone(new Vec2(350, 1090), 40, 1, 0.5, 0.5));
-
-stefan.muscles.push(new CreatureMuscle(stefan.bones[0], stefan.bones[1], 500, 800, 15));
-stefan.muscles.push(new CreatureMuscle(stefan.bones[1], stefan.bones[2], 500, 600, 100));
-stefan.muscles.push(new CreatureMuscle(stefan.bones[2], stefan.bones[0], 500, 900, 15));
-stefan.muscles.push(new CreatureMuscle(stefan.bones[1], stefan.bones[3], 50, 75, 10));
-stefan.muscles.push(new CreatureMuscle(stefan.bones[2], stefan.bones[3], 110, 110, 5));
-stefan.muscles.push(new CreatureMuscle(stefan.bones[0], stefan.bones[3], 200, 400, 25));
-
-
-let scene = new Scene();
-
-scene.addEntity(new Air(new WebImage("sky.png")));
-scene.addEntity(new Ground(new WebImage("ground.jpg")));
-scene.addEntity(stefan);
-
-let camera = new RenderTransform(TransformMatrix.translate(500, 600), scene);
 let view = new InteractiveCanvasWindow(window, 2);
-let renderer = new Renderer(view.context, camera, 1, true);
+let renderer = new Renderer(view.context, simView, 1, true);
 
 renderer.start();
-
-setInterval(
-	() => {
-		scene.update(1/60);
-		camera.transform = TransformMatrix.translate(view.width()/2 - stefan.center().x, view.height() - 150);
-	}, 1000/60
-);
