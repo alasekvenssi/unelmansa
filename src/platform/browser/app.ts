e925049e05431cc84e60f2497efb08252737f36d
Teapot418 <teapot404@gmail.com>
2016-11-27 19:47:12 +0100

    platform/browser: Use InteractiveCanvasWindow

--- src/platform/browser/app.ts
+++ src/platform/browser/app.ts
@@ -1,13 +1,14 @@
 import {Scene} from "../../core/Scene"
 import {Ground, Air} from "../../core/Entity"
 import {Creature, CreatureBone, CreatureMuscle} from "../../core/Creature"
-import CanvasWindow from "../../graphics/browser/CanvasWindow"
+import {InteractiveCanvasWindow} from "../../graphics/browser/CanvasWindow"
 import Renderer from "../../graphics/Renderer"
 import {RenderTransform} from "../../graphics/Renderable"
 import TransformMatrix from "../../util/TransformMatrix"
 import Vec2 from "../../util/Vec2"
 import Color from "../../util/Color"
 import WebImage from "../../graphics/browser/WebImage"
+import {InteractiveContext2D} from "../../graphics/InteractiveContext2D"
 
 let stefan = new Creature();
 
@@ -31,7 +32,7 @@ scene.addEntity(new Ground(new WebImage("ground.jpg")));
 scene.addEntity(stefan);
 
 let camera = new RenderTransform(TransformMatrix.translate(500, 600), scene);
-let view = new CanvasWindow(window, 2);
+let view = new InteractiveCanvasWindow(window, 2);
 let renderer = new Renderer(view.context, camera, 1, true);
 
 renderer.start();
