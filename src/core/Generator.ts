6b5613a62fd38b670d83603724ed4661ba246494
Toxyxer <toxyxer@gmail.com>
2016-11-27 23:29:13 +0100

    Changed Generator back to functions

--- src/core/Generator.ts
+++ src/core/Generator.ts
@@ -3,55 +3,51 @@ import {CreatureBone} from "./Creature"
 import Vec2 from "../util/Vec2"
 import {CreatureMuscle} from "./Creature"
 import Collide from "../physics/Collisions";
-
-export default class Generator{
 	
-	static generateCreature(): Creature {
-		while(true)
-		{
-			let generated: Creature = new Creature();
+export function generateCreature(): Creature {
+	while(true)
+	{
+		let generated: Creature = new Creature();
 
-			let numberOfBones: number = Math.floor(3*Math.random())+3;
-			for (let i = 0; i < numberOfBones; ++i) {
-				generated.bones.push(this.generateCreatureBone());
-			}
+		let numberOfBones: number = Math.floor(3*Math.random())+3;
+		for (let i = 0; i < numberOfBones; ++i) {
+			generated.bones.push(generateCreatureBone());
+		}
 
-			for (let i = 0; i < numberOfBones; ++i) {
-				for (let j = i+1; j < numberOfBones; ++j) {
-					Collide(generated.bones[i], generated.bones[j]);
-					
-					if(Math.random() >= 0.5) {
-						generated.muscles.push(this.generateCreatureMuscle(generated.bones[i], generated.bones[j]));
-					}
+		for (let i = 0; i < numberOfBones; ++i) {
+			for (let j = i+1; j < numberOfBones; ++j) {
+				Collide(generated.bones[i], generated.bones[j]);
+				
+				if(Math.random() >= 0.5) {
+					generated.muscles.push(generateCreatureMuscle(generated.bones[i], generated.bones[j]));
 				}
 			}
+		}
 
-			if(generated.isStronglyConnected()) {
-				return generated;
-			}
+		if(generated.isStronglyConnected()) {
+			return generated;
 		}
 	}
-	
-	static generateCreatureBone(): CreatureBone {
-		return new CreatureBone(
-			new Vec2(Math.random()*500 - 1000, Math.random()*500 - 1000+1500),
-			Math.random()*30+10,
-			Math.random()*10+1,
-			Math.random(),
-			Math.random()
-		);
-	}
+}
 
-	static generateCreatureMuscle(lhs: CreatureBone, rhs: CreatureBone) {
-		return new CreatureMuscle(
-			lhs,
-			rhs,
-			lhs.position.distance(rhs.position) + 50,
-			(lhs.position.distance(rhs.position) + 50) * 3,
-			Math.random() * 50 + 1,
-			Math.random() * 4 + 1,
-			Math.random()
-		);
-	}
+export function generateCreatureBone(): CreatureBone {
+	return new CreatureBone(
+		new Vec2(Math.random()*500 - 1000, Math.random()*500 - 1000+1500),
+		Math.random()*30+10,
+		Math.random()*10+1,
+		Math.random(),
+		Math.random()
+	);
+}
 
+export function generateCreatureMuscle(lhs: CreatureBone, rhs: CreatureBone) {
+	return new CreatureMuscle(
+		lhs,
+		rhs,
+		lhs.position.distance(rhs.position) + 50,
+		(lhs.position.distance(rhs.position) + 50) * 3,
+		Math.random() * 50 + 1,
+		Math.random() * 4 + 1,
+		Math.random()
+	);
 }
