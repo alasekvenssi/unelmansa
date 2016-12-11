a72fa330c2dcdd8af3a4783e29f984750453e423
Teapot418 <teapot404@gmail.com>
2016-12-11 17:18:37 +0100

    platform/browser: Show PopulationView

--- src/platform/browser/app.ts
+++ src/platform/browser/app.ts
@@ -1,7 +1,7 @@
 import { Scene } from "../../core/Scene"
 import { Ground, Air } from "../../core/Entity"
 import { Creature, CreatureBone, CreatureMuscle } from "../../core/Creature"
-import { InteractiveCanvasWindow } from "../../graphics/browser/CanvasWindow"
+import { CanvasWindow, InteractiveCanvasWindow } from "../../graphics/browser/CanvasWindow"
 import Renderer from "../../graphics/Renderer"
 import { RenderTransform } from "../../graphics/Renderable"
 import TransformMatrix from "../../util/TransformMatrix"
@@ -10,13 +10,14 @@ import Color from "../../util/Color"
 import WebImage from "../../graphics/browser/WebImage"
 import { InteractiveContext2D } from "../../graphics/InteractiveContext2D"
 import SimulationView from "../../ui/SimulationView"
+import { PopulationView } from "../../ui/PopulationView"
 import * as CoreUtil from "../../core/Util"
 
 CoreUtil.setResources(new WebImage("sky.png"), new WebImage("ground.jpg"));
 
-let simView = new SimulationView();
+let mainView = new PopulationView();
 
-let view = new InteractiveCanvasWindow(window, 2);
-let renderer = new Renderer(view.context, simView, true);
+let view = new InteractiveCanvasWindow(window, 1);
+let renderer = new Renderer(view.context, mainView, true);
 
 renderer.start();
