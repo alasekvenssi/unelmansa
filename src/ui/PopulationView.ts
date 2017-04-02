8647853a0cc5c33a733aaf7c2231475bdbd32ed8
Toxyxer <toxyxer@gmail.com>
2017-04-02 20:53:47 +0200

    ui: Added first generation to chart

--- src/ui/PopulationView.ts
+++ src/ui/PopulationView.ts
@@ -53,6 +53,7 @@ export class PopulationView extends RenderGroup implements View {
 	render(ctx: Context2D) {
 		this.populationTxt.text = "Generation: " + (this.mainView.population.generation + 1);
 		this.resultTxt.text = "Best result: " + this.bestCreature().resultWithoutPenalties.toFixed(0);
+		this.bestOfChart.addPoint(this.mainView.population.generation + 1, this.bestCreature().resultWithoutPenalties);
 		super.render(ctx);
 	}
 
